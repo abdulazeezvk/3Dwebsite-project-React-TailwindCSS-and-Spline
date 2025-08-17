83aedfe98ffecf2060ffb5cbed33046e789cb440
abdulazeezvk <abdulazeez.aa258@gmail.com>
2025-08-17 17:35:54 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/3Dwebsite-project-React-TailwindCSS-and-Spline/', // 👈 Important
 })
